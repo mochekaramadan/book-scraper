854bc5be0a926b19d9f862c7ff9fbf0cfe2983d0
Mochamad Eka Ramadan <me.ramadan14@gmail.com>
2026-09-17 21:02:10 +0700

    scrap data with pagination

--- scraper.py
+++ scraper.py
@@ -4,22 +4,39 @@ from app.scraper.client import ScraperClient
 from app.scraper.parser import parse_books
 
 URL = "https://books.toscrape.com/"
+semaphore = asyncio.Semaphore(5)
 
 async def main():
 
-    client = ScraperClient()
-    html = await client.fetch(URL)
-    books = parse_books(html)
-
+    books = await scrape_pages(1, 5)
     print(f"Found {len(books)} books")
 
-    for book in books:
-        print(f"Title: {book.title}")
-        print(f"Price: £{book.price}")
-        print(f"Availability: {book.availability}")
-        print(f"Rating: {book.rating} stars")
-        print(f"URL: {book.url}")
-        print("-" * 40)
+
+def get_page_url(page_number: int) -> str:
+    if page_number == 1:
+        return URL
+    return f"{URL}catalogue/page-{page_number}.html"
+
+async def scrape_pages(start_page: int, end_page: int):
+    client = ScraperClient()
+    tasks = [
+        scrape_page(client, page)
+        for page in range (start_page, end_page + 1)
+    ]
+    results = await asyncio.gather(*tasks)
+    books = []
+    for page_books in results:
+        books.extend(page_books)
+    return books
+
+async def scrape_page(client: ScraperClient, page: int):
+    async with semaphore:
+        url = get_page_url(page)
+        print(f"Scraping page {page}: {url}")
+        html = await client.fetch(url)
+        books = parse_books(html)
+        return books
 
 if __name__ == "__main__":
     asyncio.run(main())
+
