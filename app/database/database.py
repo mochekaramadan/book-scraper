da145b6208619fc1b6b1007c4f26330de2f1240c
Mochamad Eka Ramadan <me.ramadan14@gmail.com>
2026-09-21 22:13:01 +0700

    setup database

---
+++ app/database/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import {
+    create_engine,
+    String,
+    Float,
+    Integer
+}
+
+from sqlalchemy.orm import {
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    sessionmaker
+}
+
+DATABASE_URL = "sqlite:///books.db"
+
+class Base(DeclarativeBase):
+    pass
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    availability: Mapped[str] = mapped_column(String(255), nullable=False)
+    url: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
